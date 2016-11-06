7e3740ebdad690a84328866db1d0bf9c9eab44ea
lizheng167 <lizheng167@qq.com>
2016-11-06 14:52:09 +0800

    查询

---
+++ select.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8
+import MySQLdb
+__author__ = 'wangjj'
+
+conn = MySQLdb.connect(
+    host='127.0.0.1',
+    port=3306,
+    user='root',
+    passwd='111111',
+    db='lianxi',
+    charset='utf8'
+)
+cursor = conn.cursor()
+sql = "select * from user"
+cursor.execute(sql)
+print cursor.rowcount
+
+rs = cursor.fetchall()
+for row in rs:
+    print "user_id=%s,user_name=%s" % row
+
+cursor.close()
+conn.close()
